f10ce4a5711fb876aec3a80735d60e0c0008c7dd
resteve <resteve@zikzakmedia.com>
2013-03-05 09:06:42 +0100

    FIX download url in setup

--- setup.py
+++ setup.py
@@ -31,8 +31,8 @@ setup(name='trytond_account_es',
     description='Tryton module with Spanish chart of accounts',
     author='Tryton',
     url='http://www.tryton.org/',
-    download_url="http://downloads.tryton.org/" + \
-        VERSION.rsplit('.', 1)[0] + '/',
+    download_url=("http://downloads.tryton.org/" +
+        info.get('version', '0.0.1').rsplit('.', 1)[0] + '/'),
     package_dir={'trytond.modules.account_es': '.'},
     packages=[
         'trytond.modules.account_es',
