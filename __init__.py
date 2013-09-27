8f570688022e9aba38fa9d677627f473119e1bd8
Sergi Almacellas Abellana <sergi@koolpi.com>
2013-09-27 14:21:12 +0200

    Add recargo de equivalencia on taxes and account_kind efective

--- __init__.py
+++ __init__.py
@@ -1,3 +1,13 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 
+from trytond.pool import Pool
+from .account import *
+
+
+def register():
+    Pool.register(
+        Account,
+        Tax,
+        module='account_es', type_='model')
+
