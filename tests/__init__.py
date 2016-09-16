05b7b320d40ebf60e80373ea98144e64b354a6ca
Guillem Barba <guillem@nan-tic.com>
2016-09-16 09:23:54 +0200

    Publish create_chart function at tests module level

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,10 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 
-from .test_account_es import suite
+try:
+    from trytond.modules.account_es.tests.test_account_es import (
+        suite, create_chart)
+except ImportError:
+    from .test_account_es import suite, create_chart
 
-__all__ = ['suite']
+__all__ = ['suite', 'create_chart']
