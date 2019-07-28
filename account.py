91d667d8b5fe8631c4f31912ce44f55237e0cb7d
Albert Cervera i Areny <albert@nan-tic.com>
2019-07-28 23:37:50 +0200

    Add supplier_balance and customer_balance fields to allow using some balance accounts in invoice lines.

--- account.py
+++ account.py
@@ -2,9 +2,10 @@
 # copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import PoolMeta
+from trytond.pyson import If, Eval, Bool
 
 __all__ = ['Account', 'AccountTemplate', 'FiscalYear', 'Period',
-    'TypeTemplate']
+    'AccountTypeTemplate', 'AccountType']
 
 
 class Account(metaclass=PoolMeta):
@@ -59,9 +60,54 @@ class Period(metaclass=PoolMeta):
             ]
 
 
-class TypeTemplate(metaclass=PoolMeta):
+def AccountTypeMixin(template=False):
+
+    class Mixin:
+        supplier_balance = fields.Boolean(
+            "Supplier Balance", domain=[
+                If(Eval('statement') != 'balance',
+                    ('supplier_balance', '=', False), ()),
+                ],
+            states={
+                'invisible': Eval('statement') != 'balance',
+                },
+            depends=['statement'], help='Check to be able to use this balance '
+            'account in supplier invoice lines.')
+        customer_balance = fields.Boolean(
+            "Customer Balance", domain=[
+                If(Eval('statement') != 'balance',
+                    ('customer_balance', '=', False), ()),
+                ],
+            states={
+                'invisible': Eval('statement') != 'balance',
+                },
+            depends=['statement'], help='Check to be able to use this balance '
+            'account in customer invoice lines.')
+    if not template:
+        for fname in dir(Mixin):
+            field = getattr(Mixin, fname)
+            if not isinstance(field, fields.Field):
+                continue
+            field.states['readonly'] = (
+                Bool(Eval('template', -1)) & ~Eval('template_override', False))
+    return Mixin
+
+
+class AccountTypeTemplate(AccountTypeMixin(template=True), metaclass=PoolMeta):
     __name__ = 'account.account.type.template'
+
+    def _get_type_value(self, type=None):
+        values = super()._get_type_value(type=type)
+        if not type or type.supplier_balance != self.supplier_balance:
+            values['supplier_balance'] = self.supplier_balance
+        if not type or type.customer_balance != self.customer_balance:
+            values['customer_balance'] = self.customer_balance
+        return values
 
     @classmethod
     def check_xml_record(cls, records, values):
         return True
+
+
+class AccountType(AccountTypeMixin(), metaclass=PoolMeta):
+    __name__ = 'account.account.type'
