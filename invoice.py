9f85335d3495b91e2596839f199ea2086cbfb6f9
Bernat Brunet Torruella <bernat@nan-tic.com>
2019-11-29 17:09:52 +0100

    [#039415] Add on party account move line as a description the number of the invoice and the name of the party if it cames from an invoice. And it willl be moved to the payment if put this move on a payment group.

--- invoice.py
+++ invoice.py
@@ -1,6 +1,21 @@
 from trytond.pool import PoolMeta
 
-__all__ = ['InvoiceLine']
+__all__ = ['Invoice', 'InvoiceLine']
+
+
+class Invoice(metaclass=PoolMeta):
+    __name__ = 'account.invoice'
+
+    def _get_move_line(self, date, amount):
+        line = super(Invoice, self)._get_move_line(date, amount)
+        if line.description:
+            if self.party.name not in line.description:
+                self.description = self.party.name + ' ' + line.description
+            if self.number not in line.description:
+                self.description = self.number + ' ' + line.description
+        else:
+            line.description = self.number + ' ' + self.party.name
+        return line
 
 
 class InvoiceLine(metaclass=PoolMeta):
