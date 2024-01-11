f6fca525cf4810194f0c3324728e98423464bfe1
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2024-01-11 13:50:30 +0100

    Send only the required fields when fetching a readonly view definition. issue11134. Task #050808

--- tax.py
+++ tax.py
@@ -43,7 +43,7 @@ class TaxTemplate(metaclass=PoolMeta):
         'account.tax.template', 'Recargo Equivalencia Related Tax',
         domain=[
             ('recargo_equivalencia', '=', True),
-            ], depends=['recargo_equivalencia'],
+            ],
         help='The possible Recargo Equivalencia related to this tax')
     deducible = fields.Boolean('Deducible',
         help='Indicates if the tax is deductible')
@@ -159,7 +159,7 @@ class Tax(metaclass=PoolMeta):
         domain=[
             ('recargo_equivalencia', '=', True),
             ('company', '=', Eval('company', -1)),
-            ], depends=['recargo_equivalencia', 'company'],
+            ],
         help='The possible Recargo Equivalencia related to this tax')
     deducible = fields.Boolean('Deducible',
         help='Indicates if the tax is deductible')
