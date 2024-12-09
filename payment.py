474b52f322ff3d7d2eb9f0397cb16f2d5e2455a4
Raimon Esteve <raimonesteve@gmail.com>
2024-12-09 11:07:28 +0100

    Not add domain in case account.type is receivable or payable in account.payment,account domain

---
+++ payment.py
@@ -0,0 +1,13 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+from trytond.pool import PoolMeta
+
+
+class AccountPaymentClearing(metaclass=PoolMeta):
+    __name__ = 'account.payment'
+
+    @classmethod
+    def _account_type_domain(cls):
+        # not call super()
+        # not add domain in case account.type is receivable or payable
+        return
