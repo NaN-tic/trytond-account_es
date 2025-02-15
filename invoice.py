74ebbd7b70ffbf967ddda7c4b45167af9046d339
Bernat Brunet <bernat@nan-tic.com>
2025-02-15 18:04:50 +0100

    Add the possibility to unpay an invoice payed with the PAY button. Add a better control to allow draft an stock move. Separete the possibility by button or "internally" by other operations.
    Task: #174097

--- invoice.py
+++ invoice.py
@@ -1,14 +1,35 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
+from trytond.model import fields, ModelView, Workflow
+from trytond.pyson import Eval
+from itertools import chain
 from trytond.i18n import gettext
 from trytond.exceptions import UserError
-
-__all__ = ['Invoice', 'InvoiceLine']
 
 
 class Invoice(metaclass=PoolMeta):
     __name__ = 'account.invoice'
+
+    paid_directly = fields.Function(fields.Boolean("Has Payed directly",
+            help="The invoice has payed with the 'PAY' button"),
+        'get_paid_directly')
+
+    @classmethod
+    def __setup__(cls):
+        super().__setup__()
+        cls._buttons.update({
+                'unpay': {
+                    'invisible': ~Eval('paid_directly', False),
+                    'depends': ['paid_directly'],
+                    },
+                })
+
+    def get_paid_directly(self, name):
+        for line in chain(self.payment_lines, self.reconciliation_lines):
+            if line.move_origin == self:
+                return True
+        return False
 
     def _get_move_line(self, date, amount):
         line = super(Invoice, self)._get_move_line(date, amount)
@@ -32,6 +53,33 @@ class Invoice(metaclass=PoolMeta):
                     'account_es.msg_cancel_invoice_with_move_post'))
 
         return super(Invoice, cls).cancel(invoices)
+
+    @classmethod
+    @ModelView.button
+    @Workflow.transition('posted')
+    def unpay(cls, invoices):
+        pool = Pool()
+        Move = pool.get('account.move')
+        Reconciliation = pool.get('account.move.reconciliation')
+
+        reconciliations = []
+        moves = []
+        for invoice in invoices:
+            for line in chain(invoice.payment_lines,
+                    invoice.reconciliation_lines):
+                if line.move_origin == invoice:
+                    moves.append(line.move)
+                    # In case the move are created manually and assigned the
+                    # invoice as origin, and after reconciled manually too,
+                    # it will be removed too.
+                    if line.reconciliation:
+                        reconciliations.append(line.reconciliation)
+        if reconciliations:
+            Reconciliation.delete(reconciliations)
+        if moves:
+            moves = list(set(moves))
+            Move.draft(moves)
+            Move.delete(moves)
 
 
 class InvoiceLine(metaclass=PoolMeta):
