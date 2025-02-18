0b1d281f7a61bbb46f36061420f0505a6d308a6a
Raimon Esteve <raimonesteve@gmail.com>
2025-02-18 16:30:15 +0100

    Register invoice class when account_invoice and account_payment modules are actived

--- __init__.py
+++ __init__.py
@@ -21,8 +21,6 @@ def register():
         account.AccountTypeTemplate,
         company.Company,
         currency.Currency,
-        invoice.Invoice,
-        invoice.InvoiceLine,
         party.PartyIdentifier,
         product.Category,
         product.CategoryAccount,
@@ -59,6 +57,11 @@ def register():
     Pool.register(
         payment.AccountBankJournal,
         depends=['account_bank', 'account_payment'],
+        module='account_es', type_='model')
+    Pool.register(
+        invoice.Invoice,
+        invoice.InvoiceLine,
+        depends=['account_invoice', 'account_payment'],
         module='account_es', type_='model')
     Pool.register(
         payment.AccountPaymentClearing,
