f0f632a9ca3903586d9446b55e5afb8f1dabbe15
Raimon Esteve <raimonesteve@gmail.com>
2025-05-14 15:56:14 +0200

    ModelData not found search method; replace to get_id()

--- tests/tools.py
+++ tests/tools.py
@@ -6,25 +6,22 @@ from trytond.modules.company.tests.tools import get_company
 __all__ = ['create_chart']
 
 
-def create_chart(company=None, account_code_digits=None, config=None):
+def create_chart(company=None, account_code_digits=8, config=None):
     "Create chart of accounts"
     AccountConf = Model.get('account.configuration')
     AccountTemplate = Model.get('account.account.template', config=config)
-    ModelData = Model.get('ir.model.data')
+    ModelData = Model.get('ir.model.data', config=config)
 
     if not company:
         company = get_company()
-    data, = ModelData.find([
-            ('module', '=', 'account_es'),
-            ('fs_id', '=', 'pgc_0'),
-            ], limit=1)
 
-    if account_code_digits:
-        account_conf = AccountConf(1)
-        account_conf.default_account_code_digits = account_code_digits
-        account_conf.save()
+    chart_id = ModelData.get_id('account_es', 'pgc_0', config.context)
 
-    account_template = AccountTemplate(data.db_id)
+    account_conf = AccountConf(1)
+    account_conf.default_account_code_digits = account_code_digits
+    account_conf.save()
+
+    account_template = AccountTemplate(chart_id)
 
     create_chart = Wizard('account.create_chart')
     create_chart.execute('account')
@@ -43,16 +40,11 @@ def create_chart(company=None, account_code_digits=None, config=None):
 def get_accounts(company=None, config=None):
     "Return accounts per kind"
     Account = Model.get('account.account', config=config)
-    Data  = Model.get('ir.model.data', config=config)
+    ModelData  = Model.get('ir.model.data', config=config)
 
-    pgc_570, = Data.find([
-            ('module', '=', 'account_es'),
-            ('fs_id', '=', 'pgc_570_child'),
-            ], limit=1)
-    pgc_4700, = Data.find([
-            ('module', '=', 'account_es'),
-            ('fs_id', '=', 'pgc_4700_child'),
-            ], limit=1)
+    pgc_570_id = ModelData.get_id('account_es', 'pgc_570_child', config.context)
+    pgc_4700_id = ModelData.get_id('account_es', 'pgc_4700_child', config.context)
+
     if not company:
         company = get_company()
     accounts = {}
@@ -74,11 +66,11 @@ def get_accounts(company=None, config=None):
             ], limit=1)
     accounts['cash'], = Account.find([
             ('company', '=', company.id),
-            ('template', '=', pgc_570.db_id),
+            ('template', '=', pgc_570_id),
             ], limit=1)
     accounts['tax'], = Account.find([
             ('company', '=', company.id),
-            ('template', '=', pgc_4700.db_id),
+            ('template', '=', pgc_4700_id),
             ], limit=1)
     return accounts
 
@@ -90,7 +82,7 @@ def create_tax(rate, company=None, config=None):
     if not company:
         company = get_company()
 
-    accounts = get_accounts(company)
+    accounts = get_accounts(company, config=config)
 
     tax = Tax()
     tax.name = 'Tax %s' % rate
@@ -105,55 +97,46 @@ def create_tax(rate, company=None, config=None):
 def get_taxes(company=None, config=None):
     "Return accounts per kind"
     Account = Model.get('account.tax', config=config)
-    Data  = Model.get('ir.model.data', config=config)
+    ModelData = Model.get('ir.model.data', config=config)
 
-    iva_rep_21, = Data.find([
-            ('module', '=', 'account_es'),
-            ('fs_id', '=', 'iva_rep_21')], limit=1)
-    iva_X21_compras_bc, = Data.find([
-            ('module', '=', 'account_es'),
-            ('fs_id', '=', 'iva_X21_compras_bc')], limit=1)
-    iva_rep_10, = Data.find([
-            ('module', '=', 'account_es'),
-            ('fs_id', '=', 'iva_rep_10')], limit=1)
-    iva_X10_compras_bc, = Data.find([
-            ('module', '=', 'account_es'),
-            ('fs_id', '=', 'iva_X10_compras_bc')], limit=1)
+    iva_rep_21_id = ModelData.get_id('account_es', 'iva_rep_21', config.context)
+    iva_X21_compras_bc_id = ModelData.get_id('account_es', 'iva_X21_compras_bc', config.context)
+    iva_rep_10_id = ModelData.get_id('account_es', 'iva_rep_10', config.context)
+    iva_X10_compras_bc_id = ModelData.get_id('account_es', 'iva_X10_compras_bc', config.context)
+
     if not company:
         company = get_company()
     taxes = {}
     taxes['customer_tax'], = Account.find([
-            ('template', '=', iva_rep_21.db_id),
+            ('template', '=', iva_rep_21_id),
             ('company', '=', company.id),
             ], limit=1)
     taxes['supplier_tax'], = Account.find([
-            ('template', '=', iva_X21_compras_bc.db_id),
+            ('template', '=', iva_X21_compras_bc_id),
             ('company', '=', company.id),
             ], limit=1)
     taxes['customer_tax_10'], = Account.find([
-            ('template', '=', iva_rep_10.db_id),
+            ('template', '=', iva_rep_10_id),
             ('company', '=', company.id),
             ], limit=1)
     taxes['supplier_tax_10'], = Account.find([
-            ('template', '=', iva_X10_compras_bc.db_id),
+            ('template', '=', iva_X10_compras_bc_id),
             ('company', '=', company.id),
             ], limit=1)
     return taxes
 
-def get_tax(code, company=None):
+def get_tax(code, company=None, config=None):
     Tax = Model.get('account.tax')
-    Data = Model.get('ir.model.data')
+    ModelData = Model.get('ir.model.data', config=config)
 
     if not company:
         company = get_company()
 
-    ids = Data.find([
-        ('module', '=', 'account_es'),
-        ('fs_id', '=', code),
-        ], limit=1)
-    assert ids, f'Tax template {code} not found'
+    tax_id = ModelData.get_id('account_es', code, config.context)
+
+    assert tax_id, f'Tax template {code} not found'
     taxes = Tax.find([
-        ('template', '=', ids[0].db_id),
+        ('template', '=', tax_id),
         ('company', '=', company),
         ], limit=1)
     assert taxes, f'Tax {code} not found in company {company.rec_name}'
