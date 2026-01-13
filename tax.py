f28aa18fe6ae978a6fd4bd93f8e43d63ab414033
Bernat Brunet <bernat@nan-tic.com>
2026-01-13 14:56:12 +0100

    Update some test functions like core does.
    Remove the limitation to set 'group' to IRPF taxes.
    Exist different IRPF taxes for purchase and sale, so is better to set
    the 'group' to avoid select the incorrect tax on an invoice.

--- tax.py
+++ tax.py
@@ -3,10 +3,6 @@
 from trytond.model import fields
 from trytond.pool import PoolMeta, Pool
 from trytond.pyson import Eval
-from trytond.transaction import Transaction
-
-__all__ = ['TaxCodeTemplate', 'TaxRuleTemplate', 'TaxRuleLineTemplate',
-    'TaxTemplate', 'Tax']
 
 
 class TaxCodeTemplate(metaclass=PoolMeta):
@@ -73,35 +69,10 @@ class TaxTemplate(metaclass=PoolMeta):
 
     @classmethod
     def __register__(cls, module_name):
-        pool = Pool()
-        ModelData = pool.get('ir.model.data')
-        table = cls.__table__()
-        model_data = ModelData.__table__()
         table_h = cls.__table_handler__(module_name)
 
         surcharge_exist = table_h.column_exist('recargo_equivalencia')
         super().__register__(module_name)
-
-        transaction = Transaction()
-        cursor = transaction.connection.cursor()
-
-        # Update IRPF tax use. Migrate from old versions to +5.4
-        # Ensure that group field is null in all IRPF tax templates,
-        # becasue the IRPF account tax rules not use group any more.
-        cursor.execute(*model_data.select(
-                model_data.db_id,
-                where=(model_data.fs_id.like('irpf_%')) &
-                    (model_data.model == 'account.tax.template')))
-        for db_id, in cursor.fetchall():
-            cursor.execute(*table.select(
-                    table.group,
-                    where=table.id == db_id))
-            group, = cursor.fetchone()
-            if group:
-                cursor.execute(*table.update(
-                        [table.group], [None],
-                        where=table.id == db_id))
-
         if surcharge_exist:
             table_h.drop_column('recargo_equivalencia')
 
